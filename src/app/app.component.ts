5175154fc83c0c70bcbacad02af42ed1173c13be
Whitney Navalia <136854331+Whitney2695@users.noreply.github.com>
2024-06-12 07:18:39 -0700

    project management using angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import {SidebarComponent} from './components/sidebar/sidebar.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AdminDashboardComponent, SidebarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
